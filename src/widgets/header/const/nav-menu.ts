e58505c13040b4a62b1d9ca8b8f77e2d24cc438c
Ruslan <cmetall@rambler.ru>
2025-10-15 10:30:30 +0300

    refactoring header widget: add menu-config, decomposition JSX

---
+++ src/widgets/header/const/nav-menu.ts
@@ -0,0 +1,16 @@
+import { NavigationLink } from "@/shared/types/navigation-link.types";
+
+export const HEADER_NAV_MENU_CONFIG: NavigationLink[] = [
+    {
+        label: 'Заказы',
+        href: '/',
+    },
+    {
+        label: 'Вишлист',
+        href: '/',
+    },
+    {
+        label: 'Профиль',
+        href: '/',
+    },
+]
--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -2,33 +2,39 @@
 
 import Link from 'next/link'
 import s from './header.module.scss'
-import clsx from 'clsx'
 import { observer } from 'mobx-react'
 import { CartStore } from '@/entities/Cart'
+import { HEADER_NAV_MENU_CONFIG } from '../const/nav-menu'
 
 export const Header = observer(() => {
     const { isEmpty, totalQuantity } = CartStore;
 
+    const renderMenu = () => {
+        return (
+            <nav className={s.header_menu}>
+                <ul className={s.header_menu_list}>
+                    {HEADER_NAV_MENU_CONFIG.map(({ label, href }) => (
+                        <li key={label}>
+                            <Link href={href}>{label}</Link>
+                        </li>
+                    ))}
+                    <li>
+                        <Link href='/cart'>
+                            Корзина {!isEmpty && <span className={s.counter}>{totalQuantity}</span>}
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        )
+    }
+
     return (
         <header className={s.header}>
-            <div className={clsx(s.header_inner, 'container')}>
-                <span className={s.title}>red</span>
-                <nav className={s.header_menu}>
-                    <ul className={s.header_menu_list}>
-                        <li>
-                            <Link href='/' tabIndex={0}>Заказы</Link>
-                        </li>
-                        <li>
-                            <Link href='/'>Вишлист</Link>
-                        </li>
-                        <li>
-                            <Link href='/'>Профиль</Link>
-                        </li>
-                        <li>
-                            <Link href='/cart'>Корзина <span className={s.counter}>{!isEmpty && totalQuantity}</span></Link>
-                        </li>
-                    </ul>
-                </nav>
+            <div className='container'>
+                <div className={s.header_inner}>
+                    <span className={s.title}>red</span>
+                    {renderMenu()}
+                </div>
             </div>
         </header>
     )
